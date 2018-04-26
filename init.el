b95a6dcf91620e9b9edca6860c598c93ea9c0297
Aravind Anbudurai <aru7@fb.com>
2018-04-26 10:45:31 -0700

    org stuff

--- init.el
+++ init.el
@@ -35,7 +35,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Inconsolata for Powerline")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Inconsolata for Powerline"))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -43,4 +44,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (diminish smex evil-surround auto-yasnippet keyfreq rainbow-delimiters projectile company-irony counsel swiper ivy company evil org-bullets yasnippet-snippets yasnippet spaceline solarized-theme ace-window thrift super-save which-key use-package))))
+    (org-plus-contrib diminish smex evil-surround auto-yasnippet keyfreq rainbow-delimiters projectile company-irony counsel swiper ivy company evil org-bullets yasnippet-snippets yasnippet spaceline solarized-theme ace-window thrift super-save which-key use-package))))
